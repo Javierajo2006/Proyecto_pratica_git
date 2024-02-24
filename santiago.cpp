3534226bb3a27fa3bb2c09134f4bc9bd2176027a
santiago2908 <ejemplo@example.com>
2024-02-24 12:00:51 -0500

    Fix: funcionholamundo

--- santiago.cpp
+++ santiago.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+    int a = 10;
+    int b = 30;
 
+    funcionholamundo(10, 3);
+}
+
+void funcionholamundo(int a, int b)
+{
+    int suma = a + b;
+    cout << "Hola mundo " << suma << endl;
 }
